c2f14aeda1780b6818b1ef281c085b95a9ab2a2a
briancherus <brian.cherus@student.moringaschool.comssh-keygen -t rsa -b 4096 -C brian.cherus@student.moringaschool.com>
2023-10-26 23:34:44 +0300

    completed assignment

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+// About.js
+import React from "react";
+
+function About(props) {
+  return (
+    <aside>
+      <img src={props.image || "https://via.placeholder.com/215"} alt="blog logo" />
+      <p>{props.aboutText}</p>
+    </aside>
+  );
+}
+
+export default About;
